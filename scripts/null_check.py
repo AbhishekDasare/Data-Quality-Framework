a0e62961c56dce97e91ee7948b55d5cc71532d9d
Abhishek Dasare <107669940+AbhishekDasare@users.noreply.github.com>
2026-06-07 00:17:33 +0530

    Update null_check.py

--- scripts/null_check.py
+++ scripts/null_check.py
@@ -1,1 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
+spark = SparkSession.builder \
+    .appName("Null Check") \
+    .getOrCreate()
+
+df = spark.read.csv(
+    "data/raw_customer.csv",
+    header=True,
+    inferSchema=True
+)
+
+null_records = df.filter(
+    col("name").isNull() |
+    col("email").isNull()
+)
+
+print("Null Records Found:")
+null_records.show()
+
+spark.stop()
